f7f3b7208dc295a195f6911aa909000ff55371c9
Priyanshu5145 <agraharipriyanshu53@gmail.com>
2025-04-14 20:28:00 +0530

    add order,product,tracking controller,real time tracking

--- src/controllers/order/order.js
+++ src/controllers/order/order.js
@@ -1,4 +1,4 @@
-import Order from "../../modeels/Order.js";
+import Order from "../../models/Order.js";
 import Branch from "../../models/branch.js";
 import {Customer,DeliveryPartner} from "../../models/user.js"
 
@@ -79,5 +79,61 @@ catch(error){
 }
 
 export const updateOrderStatus=async(req,reply)=>{
-    
+ try{ const {orderId}=req.params;
+ const {status,deliveryPersonLocation}=req.body;
+ const {userId}=req.user; 
+ const deliveryPerson = await DeliveryPartner.findById(userId);
+    if(!deliveryPerson){
+        return reply.status(304).send({message : "Delivery Person not found"});
+    }
+    const order = await Order.findById(orderId);
+    if(!order) return reply.status(404).send({message:"Order not found"});
+
+
+    // jab order cancel ho gaya ya delivered to fir se arriving nh kar sakte
+    if(["cancelled","delivered"].includes(order.status)){
+        return reply.status(400).send({message:"Order cannot be updated"});
+
+    }
+  //  jo jis order pe assign h whi us order ke status ko change karega
+    if(order.deliveryPartner.toString()!==userId){
+        return reply.status(403).send({message:"Unathorised"})
+    }
+    order.status = status;
+    order.deliveryPersonLocation=deliveryPersonLocation;
+    await order.save();
+    req.server.io(orderId).emit("liveTrackingUpdates",order);
+    return reply.send(order);
+}
+catch(error){
+    return reply
+    .status(500)
+    .send({message:"Failed to update order status",error});
+}
+}
+
+export const getOrders= async(req,reply)=>{
+    try{
+        const {status,customerId,deliveryPartnerId,branchId}= req.query;
+        let query = {};
+        if (status){
+            query.status=status;
+                }
+                if(customerId){
+                    query.customer=customerId;
+                }
+               if(deliveryPartnerId){
+                query.deliveryPartner=deliveryPartnerId;
+                query.branch=branchId;
+               } 
+               const orders= await Order.find(query).populate(
+                "Customer branch items.item deliveryPartner"
+               );
+               return reply.send(orders);
+    }
+    catch(error){
+        return reply
+        .status(500)
+        .send({message:"Failed to retrieve orders",error});
+    }
 }
